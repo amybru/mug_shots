614e1b9d6427c4d39af69a6847d6a87b4689d4fd
amybru <amy.bru39@gmail.com>
2020-09-21 18:09:11 +0000

    Added navigation bar

---
+++ static/js/sidenav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("main-sidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("main-sidenav").style.width = "0";
+}
