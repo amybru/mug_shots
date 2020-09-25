01f8d64f6cddfe5e8025053816f68877a13fb29a
amybru <amy.bru39@gmail.com>
2020-09-25 15:16:39 +0000

    began template for shop page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    """A view to show all products in the database, including the option to sort and search"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+        'all_products': 'active'
+    }
+    return render(request, 'products/products.html', context)
+
