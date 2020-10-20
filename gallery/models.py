58848e36a8e59c23a42d7edddd1e2415e308257d
amybru <amy.bru39@gmail.com>
2020-10-20 21:13:47 +0000

    added reviews to gallery page

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class UserReview(models.Model):
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="user_profile",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="user_product",
+    )
+    review_title = models.CharField(max_length=200)
+    review_content = models.TextField(blank=True, null=True, default="")
+
+    class Meta:
+        ordering = ['product']
+
+    def __str__(self):
+        return self.review_title
--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,137 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+
+from profiles.models import UserProfile
+from gallery.forms import ReviewForm
+from products.models import Product
+from gallery.models import UserReview
 
 # Create your views here.
 
+
 def gallery(request):
     """Render GALLERY Page"""
-    context = {
-        'gallery_page': 'active'
-    }
+    product = Product.objects.all()
+    reviews = UserReview.objects.all()
+    review_form = ReviewForm()
+
+    if request.user.is_authenticated:
+        user = get_object_or_404(UserProfile, user=request.user)
+        if request.user == user.user:
+            context = {
+                'user': user,
+                'product': product,
+                'reviews': reviews,
+                'review_form': review_form,
+            }
+            return render(request, 'gallery/gallery.html', context)
+
+    else:
+        context = {
+            'gallery_page': 'active',
+            'product': product,
+            'reviews': reviews,
+        }
     return render(request, 'gallery/gallery.html', context)
+
+
+def new_review(request):
+
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            review_form = ReviewForm(request.POST)
+            if review_form.is_valid():
+                if len(request.POST["review_content"]) <= 0 or len(
+                        request.POST["product"]) <= 0:
+                    messages.error(
+                        request, "You haven't completed the review form! \
+                                    Please add content and try again.")
+                    return redirect(reverse("gallery"))
+                new_review = review_form.save(commit=False)
+                new_review.user_profile = user_profile
+                review_form.save()
+                messages.success(request, 'Your review has \
+                                        been added.')
+                return redirect(reverse("gallery"))
+            else:
+                messages.error(request, 'Your review could not be added. \
+                                    Please check that your review is valid.')
+
+    template = 'gallery/gallery.html'
+    context = {
+        'review_form': review_form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_review(request, review_id):
+
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    review = get_object_or_404(UserReview, id=review_id)
+    review_form = ReviewForm()
+
+    if request.user == user_profile.user:
+        if request.method == 'POST':
+            review_form = ReviewForm(request.POST, instance=review)
+            if review_form.is_valid():
+                if len(request.POST["product" or "review_content"]) <= 0:
+                    messages.error(
+                        request, "You have not completed the review form. \
+                                            Please add content and try again.")
+                    return redirect(reverse("gallery"))
+                else:
+                    review = review_form.save(commit=False)
+                    user_profile = user_profile
+                    review_form.save()
+                    messages.success(request, 'Your review has \
+                                                    been updated.')
+                    return redirect(reverse("gallery"))
+        else:
+            review_form = ReviewForm(instance=review)
+
+    template = 'gallery/gallery.html'
+    context = {
+        'review_form': review_form,
+        'user_profile': user_profile,
+        'review': review,
+    }
+
+    return render(request, template, context)
+
+
+def delete_review(request, review_id):
+
+    review = get_object_or_404(UserReview, id=review_id)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.user.is_authenticated:
+        if request.user == user_profile.user:
+            review.delete()
+            messages.success(request, 'Your review has \
+                                    been deleted.')
+            return redirect(reverse("gallery"))
+
+        elif request.user.is_superuser:
+            review.delete()
+            messages.success(request, 'You have deleted this review.')
+            return redirect(reverse("gallery"))
+
+        else:
+            messages.error(request, 'This review can only be deleted \
+                                    by the author.')
+            return redirect(reverse("gallery"))
+
+    else:
+        messages.error(request, 'You have to be signed in.')
+        return redirect(reverse("gallery"))
+
+    template = 'gallery/gallery.html'
+    context = {
+        'review': review,
+        'user_profile': user_profile,
+    }
+
+    return render(request, template, context)
