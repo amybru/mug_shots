96c1b3452f2036a3e3f21a6fb4513f090fc9f399
amybru <amy.bru39@gmail.com>
2020-10-26 20:44:23 +0000

    deploying app to heroku

--- mug_shots/settings.py
+++ mug_shots/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'rh)tmw4+f^@)!x&v$=mt=8$@n7$kqnio(nkfr4(pmrqyj%@ry#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mug-shots.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'mug_shots.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
