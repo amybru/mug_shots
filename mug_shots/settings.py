8c98d43e27ddb1d64f678327c3a4b4407a01f68b
amybru <amy.bru39@gmail.com>
2020-10-26 21:00:16 +0000

    Removed secret key and placed a new one in heroku, set debug

--- mug_shots/settings.py
+++ mug_shots/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rh)tmw4+f^@)!x&v$=mt=8$@n7$kqnio(nkfr4(pmrqyj%@ry#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mug-shots.herokuapp.com', 'localhost']
 
